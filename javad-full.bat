8e078f9979b65cef3b92913fcd3d88defc725cb6
Alex Bradley <a.bradley.dm@gmail.com>
2019-09-09 20:12:46 +0100

    merged javad and javad-full

--- javad-full.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set dir=%cd%
-if exist "%~1" set dir=%~1
-
-for /d %%D in (* .) do (
-	call javad "%%~D"\*.class -o "%%~D"
-)
--- javad.bat
+++ javad.bat
@@ -1,1 +1,10 @@
-java -jar C:\Users\Alex\bin\procyon-decompiler-0.5.30.jar %*
+@echo off
+if ".%~1"=="/full" (
+   set dir=%cd%
+   if exist "%~1" set dir=%~1
+   for /d %%D in (* .) do (
+      call %~0 "%%~D"\*.class -o "%%~D"
+   )
+) else (
+   java -jar C:\Users\Alex\bin\procyon-decompiler-0.5.30.jar %*
+)
