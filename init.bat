cf9d96e274fffc07cda384e09b65d0aa652f4a9e
Alex Bradley <a.bradley.dm@gmail.com>
2019-09-26 10:30:22 +0100

    Remove echoes

--- init.bat
+++ init.bat
@@ -1,3 +1,5 @@
+@REM DO NOT ECHO IN THIS FILE
+
 @REM The following registry key needs to point to this file:
 @REM "HKCU\Software\Microsoft\Command Processor\AutoRun".
 @REM Create if does not exist, AutoRun is type String.
@@ -8,7 +10,7 @@
 
 @REM trailing space on following line is intentional - leave it be
 prompt $P$_$G$G$G 
-echo Thank you for helping us help you help us all.
+@REM echo Thank you for helping us help you help us all.
 
 set "macro=."
 set "command=cd ."
