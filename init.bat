95aba99460eb5e3311a35770bcdad2206c6d826e
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-05 11:05:46 +0000

    Full silencing

--- init.bat
+++ init.bat
@@ -1,12 +1,13 @@
+@(
+
 @REM DO NOT ECHO IN THIS FILE
 
 @REM The following registry key needs to point to this file:
 @REM "HKCU\Software\Microsoft\Command Processor\AutoRun".
 @REM Create if does not exist, AutoRun is type String.
-@echo off
 
 @REM UTF-8 code page
-@chcp 65001 >nul
+chcp 65001 >nul
 
 @REM trailing space on following line is intentional - leave it be
 prompt $P$_$G$G$G 
@@ -17,10 +18,10 @@ set "command=cd ."
 
 @REM "ups" i.e. '..' -> 'cd ..'
 setlocal EnableDelayedExpansion
-for /l %%I in (0,1,10) do (
+for /l %%I in (0,1,10) do @(
 	doskey !macro!=!command!
-   set macro=!macro!.
-   set command=!command!\..
+	set macro=!macro!.
+	set command=!command!\..
 )
 setlocal DisableDelayedExpansion
 
@@ -33,4 +34,4 @@ doskey ps=powershell $*
 doskey brb=shutdown /r /t 0
 doskey sda=shutdown /a
 
-@echo on
+)
