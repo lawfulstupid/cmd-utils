2970a46659df77eeb8bf8fa7faf36acc55905729
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-07 15:58:01 +0000

    write to variable

--- length.bat
+++ length.bat
@@ -1,13 +1,30 @@
 @echo off
 setlocal EnableDelayedExpansion
+if "%~1" == "/?" goto help
+
 set str=%~1
 for /L %%N in (0,1,8192) do (
 	if "!str!"=="" (
-		echo %%N
-		goto:eof
+		set size=%%N
+		goto break
 	) else (
 		set str=!str:~1!
 	)
 )
-endlocal
+
+:break
+endlocal & set size=%size%
+if "%~2" == "" (
+	echo %size%
+) else (
+	set %~2=%size%
+)
+goto:eof
+
+:help
+call fx /fg:aqua
+echo LENGTH ^<string^> [^<var^>]
+call fx /r
+echo Computes the length of a string.
+echo Writes to variable if present, otherwise prints to stdout.
 goto:eof
