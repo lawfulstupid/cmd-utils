32885d44740c48863c02826c488aa1dc44b6974d
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-08 17:30:01 +0000

    Best I can do for now

--- hs.bat
+++ hs.bat
@@ -5,15 +5,10 @@
 @REM Directory to start GHCi in:
 set "hsdir=%USERPROFILE%\hs"
 @REM Location of ghc.exe:
-set "ghc=%STACK_ROOT%\programs\x86_64-windows\ghc-8.6.5\bin\ghc.exe"
+set "ghc=C:\Program Files\Haskell Stack\programs\x86_64-windows\ghc-8.6.5\bin\ghc.exe"
 
 title GHCi
 set "olddir=%cd%"
 cd "%hsdir%"
-
-@REM One of these
-@REM "%ghc%" --interactive %*
-@REM start /b /wait stack repl
 stack ghci
-
 cd "%olddir%"
