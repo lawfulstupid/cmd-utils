5aa0f4c73e7675b601cd65e62be1647ec3efd54e
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-08 14:27:03 +0000

    Now accepts multiple options

--- case.bat
+++ case.bat
@@ -1,5 +1,15 @@
-@if %~2 "%SWITCH%"=="%~1" (
-	@default
-) else (
-	@exit /b 1
-)
+@echo off
+setlocal EnableDelayedExpansion
+	set casing=
+	set options=%*
+	if /I "%~1"=="/I" (
+		set casing=/I
+		for /f "tokens=1,*" %%A in ("%*") do set options=%%B
+	)
+	
+	set found=0
+	for %%A in (!options!) do (
+		if %casing% "%switch%"=="%%~A" set found=1
+	)
+endlocal & if /I %found% EQU 1 default
+exit /b 1
