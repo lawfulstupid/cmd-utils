778780dd62fa6c66466420f90ca72301b4c4c444
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-08 15:33:54 +0000

    New utility: drop

---
+++ drop.bat
@@ -0,0 +1,23 @@
+@echo off
+if "%~1"=="/?" goto help
+if "%~2"=="" %0 1 "%~1"
+for /f "tokens=%~1,*" %%A in ("!%~2!") do (
+	set %~2=%%B
+)
+goto:eof
+
+
+:help
+setlocal EnableDelayedExpansion
+	call fx /fg:aqua
+	echo DROP [^<N^>] ^<argvar^>
+	call fx /r
+	echo Removes first [N] list item(s) from a variable.
+	echo;
+	echo Example usage:
+	echo;   set var=foo bar baz maz
+	echo;   call drop 2 var
+	echo;   echo %%var%%
+	echo;~^> baz maz
+endlocal
+goto:eof
