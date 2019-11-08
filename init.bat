686dcfff694135cf3d05066182d1905a4cb08d56
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-08 10:50:29 +0000

    Reverted previous change; messed up scoping

--- init.bat
+++ init.bat
@@ -1,4 +1,3 @@
-@(
 
 @REM DO NOT ECHO IN THIS FILE
 
@@ -7,24 +6,25 @@
 @REM Create if does not exist, AutoRun is type String.
 
 @REM UTF-8 code page
-chcp 65001 >nul
+@chcp 65001 >nul
 
 @REM trailing space on following line is intentional - leave it be
-prompt $P$_$G$G$G 
+@prompt $P$_$G$G$G 
 @REM echo Thank you for helping us help you help us all.
 
-set "macro=."
-set "command=cd ."
+@set "macro=."
+@set "command=cd ."
 
 @REM "ups" i.e. '..' -> 'cd ..'
-setlocal EnableDelayedExpansion
-for /l %%I in (0,1,10) do @(
-	doskey !macro!=!command!
-	set macro=!macro!.
-	set command=!command!\..
+@setlocal EnableDelayedExpansion
+@for /l %%I in (0,1,10) do (
+	@doskey !macro!=!command!
+	@set macro=!macro!.
+	@set command=!command!\..
 )
-setlocal DisableDelayedExpansion
+@endlocal
 
+@(
 @REM Shortcuts
 doskey x=exit
 doskey q=exit
