ed45bf2b05f3bb6f654ce3f93a8ceac8402cec2b
Alex Bradley <a.bradley.dm@gmail.com>
2019-11-12 16:12:00 +0000

    Fixed bug where empty string isn't matched

--- case.bat
+++ case.bat
@@ -5,9 +5,9 @@ setlocal EnableDelayedExpansion
 	set found=0
 	
 	:loop
-	if "%~1"=="" goto endloop
 	if %casing% "%switch%"=="%~1" set found=1
 	shift
+	if "%~1"=="" goto endloop
 	goto loop
 	:endloop
 endlocal & if /I %found% EQU 1 default
